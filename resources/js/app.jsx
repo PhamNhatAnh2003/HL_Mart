09a9305cbd8b630580db1a6674eac05f6d3e54be
PhamNhatAnh2003 <anhpham090423@gmail.com>
2025-02-27 04:58:48 +0700

    doi ten

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,14 +2,14 @@ import "./bootstrap";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
-// import GlobalStyles from "./components/GlobalStyles";
+import GlobalStyles from "./components/GlobalStyles";
 // import { AuthProvider } from "./context/AuthContext";
 
 const app = ReactDOM.createRoot(document.getElementById("app"));
 app.render(
     <React.StrictMode>
-      
+      <GlobalStyles>
                 <App />
-            
+        </GlobalStyles>
     </React.StrictMode>
 );
--- resources/js/app/App.jsx
+++ resources/js/app/App.jsx
@@ -1,4 +1,4 @@
-import Button from "../component/Button/Button";
+import Button from "../components/Button/Button";
 const App = () => {
     return (
         <Button />
